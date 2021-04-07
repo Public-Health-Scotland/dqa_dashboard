1358918bd6a97c0ebf886629f630a74bdf57aa39
Maiana Sanjuan <maians01@nssrstudio06.csa.scot.nhs.uk>
2021-04-07 16:48:23 +0100

    smr02 tidy

--- smr01_tidy_data.R
+++ smr01_tidy_data.R
@@ -1,14 +1,28 @@
 library(readxl)
-library(tidyr)
 library(dplyr)
+library(tidyr)
+library(pdftools)
+
 
 ###Load SMR0 Tables###
 SMR01_accuracy <- read_excel("~/dqa dashboard/SMR01 accuracy by data item and hospital.xls", sheet = 2)
-View(SMR01_accuracy)
+SMR02_accuracy <- read_excel("smr02_accuracy_summary_by_year.xlsx")
+
+
+###House Keeping###
+
+#rounding percentages to the same decimal point
+SMR01_accuracy$`2019/20` <- round(SMR01_accuracy$`2019/20`, 1)
+SMR02_accuracy$`2017/18`<- round(SMR02_accuracy$`2017/18`, 1)
+
+#replacing na strings with NA value
+na_index <- SMR02_accuracy$`2008/09`=="Not assessed"
+SMR02_accuracy$`2008/09`[na_index] <- NA
+
+SMR02_accuracy$`2008/09`<-as.numeric(SMR02_accuracy$`2008/09`)
 
 
 ###Convert Data to Tidy Format###
-SMR01_accuracy$`2019/20` <- round(SMR01_accuracy$`2019/20`, 1)
 
 SMR01_tidy <- SMR01_accuracy %>%
   pivot_longer(cols=2:5, names_to="Year", values_to = "Accuracy")%>%
@@ -16,4 +30,9 @@ SMR01_tidy <- SMR01_accuracy %>%
   mutate(Audit="SMR01")
 
 SMR01_tidy <- cbind(SMR01_tidy[4],SMR01_tidy[1:3])
+
+SMR02_tidy <- SMR02_accuracy %>%
+  pivot_longer(cols=2:3, names_to="Year", values_to = "Accuracy")%>%
+  rename("DataItemName"= "Data item") %>%
+  mutate(Audit="SMR02")
   
