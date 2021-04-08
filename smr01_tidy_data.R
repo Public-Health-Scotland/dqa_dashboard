dc403c7bf8219790f1ebfe9f00cc9b817d535ebf
Maiana Sanjuan <maians01@nssrstudio02.csa.scot.nhs.uk>
2021-04-08 14:29:16 +0100

    smr04 non clinical accuracy table

--- smr01_tidy_data.R
+++ smr01_tidy_data.R
@@ -1,11 +1,14 @@
 library(readxl)
 library(tidyr)
 library(dplyr)
+library(tabulizer)
 
 ###Load SMR0 Tables###
 SMR01_accuracy <- read_excel("~/dqa dashboard/SMR01 accuracy by data item and hospital.xls", sheet = 2)
 View(SMR01_accuracy)
-
+pdf_path <- "https://www.isdscotland.org/Products-and-Services/Data-Quality/docs/Assessment-of-SMR04-Data-Scotland-2015-2016.pdf"
+table1 <- extract_tables(pdf_path, output="data.frame", pages=c(29))
+SMR04_table1 <- table1[[1]]
 
 ###Convert Data to Tidy Format###
 SMR01_accuracy$`2019/20` <- round(SMR01_accuracy$`2019/20`, 1)
@@ -17,3 +20,9 @@ SMR01_tidy <- SMR01_accuracy %>%
 
 SMR01_tidy <- cbind(SMR01_tidy[4],SMR01_tidy[1:3])
 
+
+
+SMR04_table1<- SMR04_table1[c(2:9), c(1,4)] %>%
+  rename(c("DataItemName"="Non.clinical.data.item", "Accuracy"="Percentage"))%>%
+  mutate(Year="2015/16")%>%
+  mutate(Audit="SMR04")
