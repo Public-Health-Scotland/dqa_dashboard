a04534f92c511b514491546deff58022ff23e0ff
Maiana Sanjuan <maians01@nssrstudio02.csa.scot.nhs.uk>
2021-04-13 13:49:37 +0100

    data-item-list

--- smr01_tidy_data.R
+++ smr01_tidy_data.R
@@ -1,10 +1,21 @@
+
+### Description -------------------------------------------------------------
+
+#This code retrieves and tidies accuracy percentage results from national audits conducted on SMR data.
+#The percentages are averages of accuracy data produced for hospitals accross all 14 healthboards of Scotland. 
+#The data is split by data item and audit.
+
+### 1 - Libraries -----------------------------------------------------------
+
 library(readxl) #read in excel files
 library(tidyr)  
 library(dplyr)
 library(tabulizer) #extract tables from pdf documents
 library(stringr)
 
-###Load SMR0 Tables###
+
+### 2 - Load SMR Audit Data -------------------------------------------------
+
 SMR01_accuracy <- read_excel("~/dqa dashboard/SMR01 accuracy by data item and hospital.xls", sheet = 2)
 
 SMR02_accuracy <- read_excel("smr02_accuracy_summary_by_year.xlsx")
@@ -15,7 +26,8 @@ tables <- extract_tables(pdf_path, output="data.frame", pages=c(28,29))
 SMR04_df1 <- tables[[1]]
 SMR04_df2 <- tables[[2]]
 
-###House Keeping###
+
+### 3 - Housekeeping --------------------------------------------------------
 
 #rounding percentages to the same decimal point
 SMR01_accuracy$`2019/20` <- round(SMR01_accuracy$`2019/20`, 1)
@@ -28,7 +40,8 @@ SMR02_accuracy$`2008/09`[na_index] <- NA
 SMR02_accuracy$`2008/09`<-as.numeric(SMR02_accuracy$`2008/09`)
 
 
-###Convert Data to Tidy Format###
+
+# 4 - Convert Data to Tidy Format -----------------------------------------
 
 SMR01_tidy <- SMR01_accuracy %>%
   pivot_longer(cols=2:5, names_to="Year", values_to = "Accuracy")%>%
@@ -56,8 +69,16 @@ SMR04_df2<- SMR04_df2[c(2:9), c(1,4)] %>%
   mutate(Year="2015/16")%>%
   mutate(Audit="SMR04")
 
-###Bind all of our tidy dataframes together and write to csv###
+# Bind all of our tidy dataframes together
 SMR_tidy <- rbind(SMR01_tidy,SMR02_tidy,SMR04_df1,SMR04_df2)
 SMR_tidy$Accuracy <- as.numeric(SMR_tidy$Accuracy)
 
+
+# 5 - List data item name by smr ------------------------------------------
+name_smr <- SMR_tidy %>% 
+  select(Audit, DataItemName)%>%
+  distinct()
+
+# 6 - Write csv outputs ---------------------------------------------------
 write.csv(SMR_tidy, "SMR_accuracy.csv")
+write.csv(name_smr, "data_item_by_audit_list.csv")
