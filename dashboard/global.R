91c3e3525cdf9923f25f76dc788da6987f0ac92d
Maiana Sanjuan <maians01@nssrstudio06.csa.scot.nhs.uk>
2021-05-27 15:56:47 +0100

    adding more detailed data to global.R

--- dashboard/global.R
+++ dashboard/global.R
@@ -1,9 +1,13 @@
 # In the line below, import the shiny library so that it's available
 # in both ui.R and server.R
+library(readxl)
 library(shiny)
 library(tidyverse)
 library(DT)
-# In the lines below, import the files and process them. 
+
+
+# In the lines below, import the files and process them.
+
 
 SMR_accuracy <- read_csv("SMR_accuracy.csv")
 glimpse(SMR_accuracy)
@@ -41,9 +45,43 @@ dim(SMR_acc)
 
 SMR_ess <- subset(SMR_acc, select = -c(X1, DataItemName))
 
-SMR_mean <- SMR_ess %>% 
-  group_by(Year, Audit) %>% 
+SMR_mean <- SMR_ess %>%
+  group_by(Year, Audit) %>%
   dplyr::summarise(Mean = round(mean(Accuracy, na.rm = T), digits = 2)
 )
 
+
+
+
+
+
+### HB and Hospital Site Level data -----------------------------------------
+
+#set directory to dashboard folder
+setwd("~/dqa_dashboard/dashboard")
+
+#Import data
+hb_path <- "~/dqa_dashboard/smr_data/Hospital_SMR_accuracy_2004-Present.xlsx"
+
+hb_accuracy <- hb_path %>%
+  excel_sheets()%>%
+  set_names()%>%
+  map_df(~read_excel(path, sheet = .x), 
+         col_types = c("text", "text", "text", "numeric", "text"), .id = "Healthboard")%>%
+  select(c(1:6))
+
+#quick summary of hb_accuracy layout
+#keeping NA values for now, but can be removed with na.omit() if needed
+glimpse(hb_accuracy)
+colnames(hb_accuracy)
+lapply(hb_accuracy, unique)
+
+#quick formatting cleanup
+hb_accuracy[hb_accuracy$Year == "2004/2006", "Year"] <- "2004-2006"
+
+#Create a new accuracy data frame with a column with mean values grouped by SMR, Year and data item 
+hb_mean <- hb_accuracy %>%
+  group_by(Audit, Year, DataItemName)%>%
+  mutate(MeanAccuracy = round(mean(Accuracy, na.rm=TRUE),2))
+hb_mean$Accuracy <- round(hb_mean$Accuracy, 2)
 
