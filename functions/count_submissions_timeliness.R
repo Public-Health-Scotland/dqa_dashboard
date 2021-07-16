8e58e41712d06a1fbb0d360ae1038ae74b1330c0
Maiana Sanjuan <maians01@nssrstudio04.csa.scot.nhs.uk>
2021-07-16 17:40:19 +0100

    derived and collated timeliness data

---
+++ functions/count_submissions_timeliness.R
@@ -0,0 +1,28 @@
+count_submissions <- function(data, date_col, deadlines){
+  
+  df_list <- list()
+  months <- data %>%
+    distinct(month({{date_col}})) %>% 
+    pull()
+  
+  
+  for(i in months){
+    submissions <- data %>% 
+      mutate(event_year=year({{date_col}}), 
+             event_month=month({{date_col}}, label=FALSE),
+             event_month_name=month({{date_col}}, label=TRUE))%>%
+      group_by(hbres_currentdate, event_year,event_month, event_month_name) %>%
+      filter(event_month==i) %>% 
+      summarize(
+        total_submissions = n(),
+        on_time = sum(date_record_inserted <= deadlines[i]), 
+        late = sum(date_record_inserted > deadlines[i])
+      )%>%
+      ungroup()
+    
+    df_list[[i]] <- submissions
+    
+  }
+  
+  return(do.call(rbind,df_list))
+}
