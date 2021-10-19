34a4843729b4adbc237721c1c979a200e2728dfb
Maiana Sanjuan <maians01@nssrstudio07.csa.scot.nhs.uk>
2021-10-19 09:20:48 +0100

    removed credentials.R file

--- admin/create_credentials.R
+++
@@ -1,9 +0,0 @@
-# Code to create the credential files
-dir.create("admin/") # creates folder if it doesn't exist
-
-credentials_df <- data.frame(
-  user = c("username"), # mandatory
-  password = c("password"), # mandatory
-  stringsAsFactors = FALSE)
-
-saveRDS(credentials_df, "admin/credentials.rds")
