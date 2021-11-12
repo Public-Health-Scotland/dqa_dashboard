d2bc0cd7babe79cbe8ffd8f68ef2406f1c41b389
Maiana Sanjuan <maians01@nssrstudio06.csa.scot.nhs.uk>
2021-11-12 13:21:11 +0000

    update script now updates smr01 clinical coding data

--- code/clinical_coding_tab/terminology_tables_smr01.R
+++ code/clinical_coding_tab/terminology_tables_smr01.R
@@ -4,35 +4,12 @@ library(dplyr)
 library(janitor)
 library(stringr)
 
-###Open a connection to the SMRA database
-# con <- dbConnect(odbc(), dsn = "SMRA", uid = .rs.askForPassword("SMRA Username:"), 
-#                  pwd = .rs.askForPassword("SMRA Password:"))
-
-###Extract the main_condition codes for the desired time period:
-#hbres_currentdate condains the patient's health board of treatment
+###Extract patient treatment data for the last episode of multi-episode Continuous Inpatient Stays(CIS)
+#hbtreat_currentdate is the patient's health board of treatment
 #link_no and cis_marker are used to order Continuous Inpatient Stay (CIS) episodes
 #main_condition contains the main ICD10 clinical code for an episode
 
-diagnosis1 <- dbGetQuery(con, "SELECT hbtreat_currentdate, discharge_date, link_no, 
-                         cis_marker, main_condition
-                         FROM analysis.smr01_pi
-                         WHERE discharge_date BETWEEN {d TO_DATE('2021-01-01', 'YYYY-MM-DD')} 
-                 AND {d TO_DATE('2021-06-30', 'YYYY-MM-DD')}
-                         ORDER BY link_no, cis_marker, admission_date, record_type, discharge_date, admission, discharge, uri") %>%
-  clean_names()
-
-###Filter the last episode of every multi-episode CIS set 
-#(we're looking at sets where there's more than 1 episode and the patient has been transfered)
-last_episode <- diagnosis1 %>%
-  group_by(link_no, cis_marker)%>%
-  mutate(epinum = dplyr::row_number(), last_epi = max(epinum))%>% #generate episode number and last_episode
-  filter(epinum == last_epi & last_epi > 1) %>% #filter through the last episode record for multi-episode stays (ie. last episode > 1)
-  mutate(r_code = case_when(str_detect(main_condition, "^R") ~ 1, 
-                            TRUE ~ 0))
-
-#developing a sql script that can filter out all the last CIS episodes
-#for mult-episode stays to limit ammount of data getting pulled into r
-last_episode2 <- dbGetQuery(con,
+last_episode <- dbGetQuery(con,
 "WITH
 cis_data AS(
   SELECT
@@ -42,7 +19,7 @@ cis_data AS(
   FROM
     analysis.smr01_pi
   WHERE
-    discharge_date BETWEEN {d to_date('2021-01-01', 'YYYY-MM-DD')} AND {d to_date('2021-06-30', 'YYYY-MM-DD')})
+    discharge_date BETWEEN {d to_date('2017-01-01', 'YYYY-MM-DD')} AND SYSDATE)
 SELECT *
 FROM 
   (SELECT hbtreat_currentdate, discharge_date, link_no, cis_marker, main_condition, epinum,
@@ -53,70 +30,33 @@ FROM
 WHERE epinum = last_epi") %>%
   clean_names()
 
-
-rank_test <- diagnosis1 %>%
-  group_by(hbtreat_currentdate, link_no, cis_marker)%>%
-  mutate(epinum = dplyr::row_number(discharge_date), last_epi = max(epinum)) %>% 
-  arrange(hbtreat_currentdate, link_no, cis_marker,discharge_date) %>% 
-  ungroup()
-
-  
-
 #Read in hb_lookup file:
 hb_lookup <- read_csv(here::here("lookups", "hb_lookup.csv"))
 
-#append hb names to diagnosis2 dataframe
+#append hb names, create flag column for R codes in main condition, create year column for discharge year 
 last_episode1 <- left_join(last_episode, hb_lookup[c(1:2)], 
-                           by = c("hbtreat_currentdate"="HB"))
+                           by = c("hbtreat_currentdate"="HB")) %>% 
+  mutate(r_code = if_else(str_detect(main_condition, "^R"), 1, 0),
+         year = (substr(discharge_date, 1, 4)))
 
-last_episode2 <- last_episode1 %>% 
-  mutate(
-    year = (substr(discharge_date, 1, 4))
-  )
-
-all_multi_episodes <- last_episode2 %>% 
+all_multi_episodes <- last_episode1 %>% 
   group_by(HBName, year) %>%
   summarise(n()) %>% 
   ungroup()
-  
-RCodes <- last_episode2 %>% 
-  group_by(HBName, year) %>% 
-  filter(r_code == 1) %>%
-  filter(!is.na(HBName)) %>% 
-  mutate(
-    resp_chest = case_when(
-      str_detect(main_condition, c("^R05", "^R06", "^R07")) ~ "resp/chest",
-      TRUE ~ 'other')) %>% 
-  mutate(
-    APV = case_when(
-      str_detect(main_condition, "^R10") | str_detect(main_condition, "^R11") ~ 'AP&V',
-      TRUE ~ 'other'
-    )) %>% 
-  mutate(
-    collapse_convuls = case_when(
-      str_detect(main_condition, "^R55") | str_detect(main_condition, "^R56") ~ 'collapse/convuls',
-      TRUE ~ 'other')) %>% 
-  summarise(
-    resp_chest = sum(resp_chest == 'resp/chest'), APV = sum(APV == 'AP&V'), collapse_convuls = sum(collapse_convuls == 'collapse/convuls'), 
-    all = sum(r_code == 1) 
-  )
 
-#summarise counts by clinical code groupings
-RCodes2 <- last_episode2 %>% 
+#summarise R code counts by clinical code groupings 
+RCodes <- last_episode1 %>% 
   group_by(HBName, year) %>% 
   filter(r_code == 1, !is.na(HBName)) %>%
-  mutate(group == case_when(
-    str_detect(main_condition, c("^R05", "^R06", "^R07")) ~ "resp/chest",
-    str_detect(main_condition, c("^R10","^R11")) ~ "AP&V",
-    str_detect(main_condition, c("^R55", "^R56")) ~ "collapse/convuls"
+  mutate(group = case_when(
+    str_detect(main_condition, "^(R05|R06|R07)") ~ "resp/chest",
+    str_detect(main_condition, c("^(R10|R11)")) ~ "AP&V",
+    str_detect(main_condition, c("^(R55|R56)")) ~ "collapse/convuls",
     TRUE ~ "other"
-    )) %>% 
+  )) %>% 
   summarise(resp_chest = sum(group == "resp/chest"), APV = sum(group == "AP&V"),
             collapse_convuls = sum(group == 'collapse/convuls'), all = sum(r_code == 1))
-  
 
 RCodes_multi <- left_join(RCodes, all_multi_episodes)
 
-write_csv(RCodes_multi, "/conf/Data_Quality_Dashboard/data/RCodes.csv")
-
- 
+write_csv(RCodes_multi, "/conf/Data_Quality_Dashboard/data/r_codes.csv")
