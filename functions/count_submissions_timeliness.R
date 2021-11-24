fb7450313b6fef36f7e9145dd75e44b84d64f1f3
Maiana Sanjuan <maians01@nssrstudio07.csa.scot.nhs.uk>
2021-11-24 18:35:06 +0000

    made the timeliness script more flexible in handling of dates

--- functions/count_submissions_timeliness.R
+++ functions/count_submissions_timeliness.R
@@ -1,28 +1,21 @@
-count_submissions <- function(data, date_col, deadlines){
+count_submissions <- function(smr_data){
+
+  sub_list <- vector(mode = "list", length = 4)
   
-  df_list <- list()
-  months <- data %>%
-    distinct(month({{date_col}})) %>% 
-    pull()
-  
-  
-  for(i in months){
-    submissions <- data %>% 
-      mutate(event_year=year({{date_col}}), 
-             event_month=month({{date_col}}, label=FALSE),
-             event_month_name=month({{date_col}}, label=TRUE))%>%
-      group_by(hbres_currentdate, event_year,event_month, event_month_name) %>%
-      filter(event_month==i) %>% 
+  for(i in 1:length(sub_list)){
+    sub_list[[i]] <- smr_data[[i]] %>% 
+      mutate(event_year = lubridate::year(event_date), 
+             event_month = lubridate::month(event_date, label=FALSE),
+             event_month_name = lubridate::month(event_date, label=TRUE))%>%
+      group_by(smr, hbtreat_currentdate, event_year,event_month, event_month_name) %>%
       summarize(
         total_submissions = n(),
-        before_deadline = sum(date_record_inserted <= deadlines[i]), 
-        after_deadline = sum(date_record_inserted > deadlines[i])
+        before_deadline = sum(date_record_inserted <= sub_deadline), 
+        after_deadline = sum(date_record_inserted > sub_deadline)
       )%>%
       ungroup()
     
-    df_list[[i]] <- submissions
-    
   }
   
-  return(do.call(rbind,df_list))
+  return(do.call(rbind,sub_list))
 }
