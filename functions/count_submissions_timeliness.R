055dff2a01a6b942b7b8927284b51edcf75e975c
maiana-sanjuan <81360390+maiana-sanjuan@users.noreply.github.com>
2021-12-08 09:36:01 +0000

    Update functions/count_submissions_timeliness.R
    Co-authored-by: Russell McCreath <33964310+rmccreath@users.noreply.github.com>

--- functions/count_submissions_timeliness.R
+++ functions/count_submissions_timeliness.R
@@ -5,8 +5,8 @@ count_submissions <- function(smr_data){
   for(i in 1:length(sub_list)){
     sub_list[[i]] <- smr_data[[i]] %>% 
       mutate(event_year = lubridate::year(event_date), 
-             event_month = lubridate::month(event_date, label=FALSE),
-             event_month_name = lubridate::month(event_date, label=TRUE))%>%
+             event_month = lubridate::month(event_date, label = FALSE),
+             event_month_name = lubridate::month(event_date, label = TRUE)) %>%
       group_by(smr, hbtreat_currentdate, event_year,event_month, event_month_name) %>%
       summarize(
         total_submissions = n(),
