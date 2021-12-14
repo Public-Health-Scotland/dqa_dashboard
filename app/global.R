aabfa9a33674185128dfd5975f2ceed2e2adc1cc
Maiana Sanjuan <maians01@nssrstudio07.csa.scot.nhs.uk>
2021-12-14 13:24:35 +0000

    update filepaths in global file

--- app/global.R
+++ app/global.R
@@ -30,26 +30,20 @@ library(shinymanager)
 #Read in Data -----------------------------------------------------------
 
 #smr timeliness data
-timeliness <- read_csv(here::here("app/data","timeliness.csv"))
+timeliness <- read_csv(here::here("data","timeliness.csv"))
 
 #smr completeness table with sparkline plot html
-smr_completeness <- read_csv(here::here("app/data", "smr_completeness.csv")) %>%
+smr_completeness <- read_csv(here::here("data", "smr_completeness.csv")) %>%
   select(-hbtreat_currentdate)
 
 #record of date range used to produced sparkline plots in completeness table
-comp_barchart_dates <- read_csv(here::here("app/data", "comp_barchart_dates.csv"))
+comp_barchart_dates <- read_csv(here::here("data", "comp_barchart_dates.csv"))
 
 #smr audit data
-smr_audit <- read_csv(here::here("app/data", "dashboard_smr_audit_data.csv"))
+smr_audit <- read_csv(here::here("data", "dashboard_smr_audit_data.csv"))
 
 #smr01 clinical coding data
-RCodes_table <- read.csv(here::here("app/data", "r_codes.csv"))
+RCodes_table <- read.csv(here::here("data", "r_codes.csv"))
 
 #smr02 clinical coding data
-smr02_diabetes <- read_csv(here::here("app/data", "smr02_diabetes_data.csv"))
-
-
-
-
-
-
+smr02_diabetes <- read_csv(here::here("data", "smr02_diabetes_data.csv"))
